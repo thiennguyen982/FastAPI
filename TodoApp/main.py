399fbcf667194d15b2ea3f372cc8ac682eb307db
nyu9hc <nguyenhoangthien1998@gmail.com>
2024-10-27 17:12:55 +0700

    Changing run-command

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 from models import models
 from database import engine
 from routers import todos, auth, admin, users
@@ -11,3 +12,6 @@ app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
 app.include_router(users.router)
+
+if __name__ == "__main__":
+    uvicorn.run(host="0.0.0.0", port=8000, app=app)
